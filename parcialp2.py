1d3c3d54f5260ec3c2f9cc4671e40697c7bc8ab7
dragon0693 <dragon0693@outlook.com.pe>
2019-01-22 22:00:15 -0500

    example
    example-of-python

---
+++ parcialp2.py
@@ -0,0 +1,12 @@
+### Cualador de temperatura.
+
+T = input("Yo yo te puedo ayudar a saber si estas en un lugar que hace frio, esta fresco o es un lugar caluroso. Para eso nescesito que me dias la temperatura que muestra el lugar donde estas : ")
+
+
+### clasificamos la temperatura con C
+if T <= 15 :
+	print("hace frio")
+if T > 16 and T < 29 :
+	print("esta fresco")
+if T >= 29 :
+	print("hace calor")
